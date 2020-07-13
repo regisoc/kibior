26c107ca50a8153270e69100748e531a88ffe35e
regisoc <regis.ongaro-carcy.1@ulaval.ca>
2020-07-13 17:11:25 -0400

    Add: private test - round

--- tests/testthat/test-private-methods.R
+++ tests/testthat/test-private-methods.R
@@ -5,48 +5,52 @@ context("Private methods - utils")
 
 
 # helper class to test private members
-# https://stackoverflow.com/a/44642256
+# inherits from Kibior class
+# makes the privates public for testing
 PrivateMethodsTester <- R6Class(
-  inherit = Kibior,
-  public = list(
-    test_define_mappings = function(x){
-      private$define_mappings(x)
-    }
-  )
+    inherit = Kibior,
+    public = list(
+        test_round = function(x, nb_decimal = 1L){
+            private$round(x, nb_decimal = nb_decimal)
+        }
+    )
 )
+#
 instantiate <- function(){
-  do.call(PrivateMethodsTester$new, es_args)
+    do.call(PrivateMethodsTester$new, .kibior_get_elastic_var_from_env())
 }
 
 
-# start define_mappings ----
+# start round ----
 
-test_that("kibior::define_mappings, wrong args", {
-  remove_all_indices()
-  private_define_mappings <- instantiate()$test_define_mappings
-  #
-  expect_error(private_define_mappings())
-  expect_error(private_define_mappings(c()))
-  expect_error(private_define_mappings("aaa"))
-  expect_error(private_define_mappings(c("aaa", "bbb")))
-  expect_error(private_define_mappings(1))
-  expect_error(private_define_mappings(c(1, 2)))
-  expect_error(private_define_mappings(list()))
+test_that("kibior::round, wrong args", {
+    private_round <- instantiate()$test_round
+    # nb
+    expect_error(private_round())
+    expect_error(private_round(NULL))
+    expect_error(private_round(c()))
+    expect_error(private_round("aaa"))
+    expect_error(private_round(c("aaa", "bbb")))
+    expect_error(private_round(list()))
+    # nb_decimal
+    expect_error(private_round(1.1234, nb_decimal = NULL))
+    expect_error(private_round(1.1234, nb_decimal = c()))
+    expect_error(private_round(1.1234, nb_decimal = "aaa"))
+    expect_error(private_round(1.1234, nb_decimal = c("aaa", "bbb")))
+    expect_error(private_round(1.1234, nb_decimal = list()))
+    expect_error(private_round(1.1234, nb_decimal = -2))
 })
 
-test_that("kibior::define_mappings, nominal case", {
-  remove_all_indices()
-  res <- kc$push(starwars, single_index_name)
-  expect_equal(res, single_index_name)
-  private_define_mappings <- instantiate()$test_define_mappings
-  #
-  m <- private_define_mappings(starwars)$properties
-  p <- kc$mappings(single_index_name)[[single_index_name]][[single_index_name]]$properties
-  expect_setequal(c(names(m), "kid"), names(p))
-  for(pro in names(m)){
-    expect_equal(p[[pro]]$type, m[[pro]]$type)
-  }
+test_that("kibior::round, nominal case, round decimal", {
+    private_round <- instantiate()$test_round
+    #
+    expect_equal(typeof(private_round(1.12314)), "double")
+    expect_identical(private_round(1.12314, nb_decimal = 0), 1)
+    expect_identical(private_round(1.12314, nb_decimal = 1), 1.1)
+    expect_identical(private_round(1.12314, nb_decimal = 2), 1.12)
+    # based on 1L
+    expect_identical(private_round(1.12314), 1.1)
 })
 
-# end define_mappings
+# end round
 
